c67f0304cc6cd7a5f52a020e9109528f13a8d996
zcvfcat <zcvfcat@gmail.com>
2022-11-16 23:36:28 +0900

    feat (bellman-ford) i don't know this

--- 6. 그래프/벨만-포드/_벨만-포드_연습.py
+++ 6. 그래프/벨만-포드/_벨만-포드_연습.py
@@ -19,6 +19,23 @@ def bellman_ford(start_node):
     for node_index in range(width):
         for edge_index in range(edge_range):
             node, edge, weight = edges[edge_index]
-
             cost = distance[node] + weight
-            
+
+            if distance[node] != sys.maxsize and distance[edge] > cost:
+                distance[edge] = cost
+
+                if node_index == width:
+                    return True
+    return False
+
+
+isCycle = bellman_ford(start_node)
+
+if isCycle:
+    print("-1")
+else:
+    for node in range(2, width + 1):
+        if distance[node] == sys.maxsize:
+            print("-1")
+        else:
+            print(distance[node])
