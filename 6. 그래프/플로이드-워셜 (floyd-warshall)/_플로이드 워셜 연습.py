5be28cedcd8b602b049f044ce0eac2d375534f0c
zcvfcat <zcvfcat@gmail.com>
2022-11-17 21:49:18 +0900

    feat (floyd-warshall) base

---
+++ 6. 그래프/플로이드-워셜 (floyd-warshall)/_플로이드 워셜 연습.py
@@ -0,0 +1,44 @@
+import sys
+
+edges = [[1, 2, 2],
+         [1, 3, 3],
+         [1, 4, 1],
+         [1, 5, 10],
+         [2, 4, 2],
+         [3, 4, 1],
+         [3, 5, 1],
+         [4, 5, 3],
+         [3, 5, 10],
+         [3, 1, 8],
+         [1, 4, 2],
+         [5, 1, 7],
+         [3, 4, 2],
+         [5, 2, 4], ]
+
+node_range = 5
+edge_range = len(edges)
+distance = [[sys.maxsize for j in range(node_range + 1)] for i in range(node_range + 1)]
+
+# 초기화
+for i in range(1, node_range + 1):
+    distance[i][i] = 0
+
+# 노드 인접 엣지 업데이트
+for node, edge, weight in edges:
+    if distance[node][edge] > weight:
+        distance[node][edge] = weight
+
+# 플로이드-워셜 수행
+
+for mid_node in range(1, node_range):
+    for node in range(1, node_range):
+        for edge in range(1, node_range):
+            distance[node][edge] = min(distance[node][edge], distance[node][mid_node] + distance[mid_node][edge])
+
+for node in range(1, node_range + 1):
+    for edge in range(1, node_range + 1):
+        if distance[node][edge] == sys.maxsize:
+            print(0, end='\t')
+        else:
+            print(distance[node][edge], end='\t')
+    print()
--- 6. 그래프/플로이드-워셜 (floyd-warshall)/플로이드.11404.back.py
+++ 6. 그래프/플로이드-워셜 (floyd-warshall)/플로이드.11404.back.py
@@ -1,30 +1,32 @@
-# 모르겠는데?
 import sys
-input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
+node_range = int(input())
+edge_range = int(input())
 
-distance = [[sys.maxsize for _ in range(n + 1)] for _ in range(n + 1)]
+# 초기화
+distance = [[sys.maxsize for _ in range(node_range + 1)] for _ in range(node_range + 1)]
 
-for i in range(1, n + 1):
-    distance[i][i] = 0
+# 자기자신 업데이트
+for node in range(1, node_range + 1):
+    distance[node][node] = 0
 
-for i in range(m):
-    s, e, v = map(int, input().split())
-    if distance[s][e] > v:
-        distance[s][e] = v
+# 노드 인접 엣지 업데이트
+for i in range(edge_range):
+    node, edge, weight = map(int, input().split())
+    if distance[node][edge] > weight:
+        distance[node][edge] = weight
 
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if distance[i][j] > distance[i][k] + distance[k][j]:
-                distance[i][j] = distance[i][k] + distance[k][j]
+# 플루이드 워셜 점화식
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if distance[i][j] == sys.maxsize:
+for mid_node in range(1, node_range + 1):
+    for node in range(1, node_range + 1):
+        for edge in range(1, node_range + 1):
+            distance[node][edge] = min(distance[node][edge], distance[node][mid_node] + distance[mid_node][edge])
+
+for node in range(1, node_range + 1):
+    for edge in range(1, node_range + 1):
+        if distance[node][edge] == sys.maxsize:
             print(0, end=' ')
         else:
-            print(distance[i][j], end=' ')
+            print(distance[node][edge], end=' ')
     print()
