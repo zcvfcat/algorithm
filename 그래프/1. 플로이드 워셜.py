b460ebfedc2dc37333e298f996598d7627fc2ac4
김대현 <zcvfcat@gmail.com>
2022-12-29 12:41:43 +0900

    feat (graph)

--- 그래프/1. 플로이드 워셜.py
+++ 그래프/1. 플로이드 워셜.py
@@ -1,21 +1,19 @@
-import sys
-
+INF = float('inf')
 node_length = 4
 edges = [[1, 3, -2], [2, 1, 4], [2, 3, 3], [3, 4, 2], [4, 2, -1]]
 
 
 def floyd_warshall(node_length, edges):
-    d = [[sys.maxsize if w != h else 0 for w in range(node_length + 1)] for h in range(node_length + 1)]
+    d = [[INF if w != h else 0 for w in range(node_length + 1)] for h in range(node_length + 1)]
 
     for node, edge, cost in edges:
         d[node][edge] = cost
 
     for route in range(1, node_length + 1):
-        for node in range(1, node_length + 1):
+        for node in range(1, node_length  + 1):
             for edge in range(1, node_length + 1):
                 d[node][edge] = min(d[node][edge], d[node][route] + d[route][edge])
-
+    
     return d
-
 
 print(floyd_warshall(node_length, edges))
