a79acc6894ed1c4966cec396060d2d5c2e34fa1b
김대현 <zcvfcat@gmail.com>
2022-12-29 14:23:56 +0900

    feat (graph)

---
+++ .test/프림 알고리즘 복습.py
@@ -0,0 +1,33 @@
+import heapq
+
+node_length = 3
+edge_length = 3
+
+graph = [
+    [],
+    [(1, 2), (3, 3)],
+    [(2, 3), (1, 1)],
+    [(3, 1), (2, 2)],
+]
+
+visited = [False for _ in range(node_length + 1)]
+
+
+def prim(start_node):
+    q = [(0, start_node)]
+    passed_cost = 0
+
+    while q:
+        cost, node = heapq.heappop(q)
+
+        if not visited[node]:
+            visited[node] = True
+            passed_cost += cost
+
+            for cost, edge in graph[node]:
+                heapq.heappush(q, (cost, edge))
+
+    return passed_cost
+
+print(visited)
+print(prim(1))
