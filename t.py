3ac4a094162c34f3cb3b88ee7e208661dff52c03
김대현 <zcvfcat@gmail.com>
2023-01-17 09:01:14 +0900

    feat (dijkstra)

--- t.py
+++ t.py
@@ -1,2 +1,29 @@
-if all(True == element for element in [True, True]):
-    print('a')
+from itertools import permutations
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def solution(numbers):
+    lst = []
+    for l in range(1, len(numbers)):
+        k = [*map(lambda x: ''.join(x), permutations(numbers, l))]
+        lst.extend(k)
+
+    count = 0
+    print(lst)
+    for k in set(map(lambda k: int(k), lst)):
+        if is_prime(k):
+            count += 1
+    return count
+
+
+print(solution("011"))
