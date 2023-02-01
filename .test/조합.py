98dcb7df446defc947d04ce6aa90ce9b1faa3e5d
김대현 <zcvfcat@gmail.com>
2023-02-01 09:04:47 +0900

    feat (combination)

--- .test/조합.py
+++ .test/조합.py
@@ -4,26 +4,24 @@ def permutation(array, tie):
     if tie == 0:
         return [ans]
 
-    for idx in range(len(array)):
-        node = array[idx]
-
+    for idx, node in enumerate(array):
         for edges in permutation(array[:idx] + array[idx + 1:], tie - 1):
             ans.append((node, *edges))
-    
+
     return ans
 
-def combination(array,tie):
+
+def combination(array, tie):
     ans = []
 
     if tie == 0:
         return [ans]
 
-    for idx in range(len(array)):
-        node = array[idx]
-
-        for edges in combination(array[idx + 1: ], tie - 1):
+    for idx, node in enumerate(array):
+        for edges in combination(array[idx + 1:], tie - 1):
             ans.append((node, *edges))
     return ans
+
 
 print(permutation([1, 2, 3], 2))
 print(combination([1, 2, 3], 2))
