cb0d6aebc5188744d2296f8fe9064753783df914
zcvfcat <zcvfcat@gmail.com>
2023-02-25 18:06:05 +0900

    feat (srtf)

--- 시뮬레이션/cpu/SRTF.py
+++ 시뮬레이션/cpu/SRTF.py
@@ -3,9 +3,20 @@ jobs = [(0, 3), (1, 9), (2, 6)]
 
 
 def SRTF(jobs):
-    jobs.sort()
-    q = []
+    # 입력 받은 리스트를 힙 데이터 구조로 변환
+    heap = []
+    for job in jobs:
+        heapq.heappush(heap, job)
 
-    while jobs or q:
-        if jobs
-        pass
+    t = 0
+    result = []
+    # 힙 데이터 구조가 빌 때까지 반복
+    while heap:
+        job = heapq.heappop(heap)
+        t += job[1]
+        result.append((job[0], t))
+    return result
+
+
+result = SRTF(jobs)
+print(result)
