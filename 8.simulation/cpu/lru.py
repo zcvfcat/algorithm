23272af29e2ec0a8dbf5e94b37bc3af4ea6d3799
zcvfcat <zcvfcat@gmail.com>
2023-03-04 23:51:56 +0900

    feat (cache)

---
+++ 8.simulation/cpu/lru.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+cache_size = 3
+jobs = [4, 2, 3, 4, 5, 6, 5, 4, 7]
+
+cache = deque()
+
+for job in jobs:
+    if job not in cache:
+        if len(cache) < cache_size:
+            cache.append(job)
+        else:
+            cache.popleft()
+            cache.append(job)
+    else:
+        del cache[cache.index(job)]
+        cache.append(job)
+
+print(cache)
