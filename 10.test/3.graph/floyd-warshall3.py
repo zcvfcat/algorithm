f6b53bdd45384f56bb5a01e6c5c922771d72cafa
김대현 <zcvfcat@gmail.com>
2023-04-20 09:02:09 +0900

    feat (warm-up)

---
+++ 10.test/3.graph/floyd-warshall3.py
@@ -0,0 +1,30 @@
+from math import inf
+
+
+def floyd_warshall(edges: list, size: int):
+    size += 1
+
+    distances = [[inf for _ in range(size)] for _ in range(size)]
+
+    for y in range(size):
+        for x in range(size):
+            if y == x:
+                distances[y][x] = 0
+
+    for node, edge, cost in edges:
+        distances[node][edge] = cost
+
+    for route in range(size):
+        for node in range(size):
+            for edge in range(size):
+                distances[node][edge] = min(distances[node][edge],
+                                            distances[node][route]+distances[route][edge])
+    return distances
+
+
+node_length = 4
+edges = [[1, 3, -2], [2, 1, 4], [2, 3, 3], [3, 4, 2], [4, 2, -1]]
+
+distances = floyd_warshall(edges, node_length)
+for distance in distances:
+    print(distance)
