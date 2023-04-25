eb480a51ffcbbe8c38af1ce2eda5d099436d3dd5
김대현 <zcvfcat@gmail.com>
2023-04-26 08:56:56 +0900

    feat (tim-sort)

--- 1.sort/nlogn/tim-sort.py
+++ 1.sort/nlogn/tim-sort.py
@@ -1,60 +1,67 @@
-def insertion_sort(arr, left, right):
-    """
-    A simple implementation of insertion sort
-    """
-    for i in range(left+1, right+1):
-        key_item = arr[i]
-        j = i - 1
-        while j >= left and arr[j] > key_item:
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = key_item
+def insert_sort(array: list, start, end):
+    for node in range(start, end):
+        idx = node
 
-def merge(arr, start, midpoint, end):
-    """
-    Merge function for merging two sorted runs
-    """
-    first_half = arr[start:midpoint+1]
-    last_half = arr[midpoint+1:end+1]
-    merged_arr, i, j = [], 0, 0
+        while idx >= start and array[idx] > array[idx + 1]:
+            array[idx + 1], array[idx] = array[idx], array[idx + 1]
+            idx -= 1
 
-    while (i < len(first_half) and j < len(last_half)):
-        if (first_half[i] < last_half[j]):
-            merged_arr.append(first_half[i])
-            i += 1
+    return array
+
+
+def merge(array, start, mid, end):
+    left = array[start: mid]
+    right = array[mid: end]
+
+    sorted_array = []
+
+    while left and right:
+        if left[0] < right[0]:
+            sorted_array.append(left.pop(0))
         else:
-            merged_arr.append(last_half[j])
-            j += 1
+            sorted_array.append(right.pop(0))
 
-    merged_arr += first_half[i:]
-    merged_arr += last_half[j:]
+    sorted_array += left
+    sorted_array += right
 
-    return merged_arr
+    return sorted_array
 
-def tim_sort(arr):
-    """
-    Tim Sort algorithm implementation
-    """
-    # define a minimum run
-    min_run = 32
-    n = len(arr)
-    
-    # Insertion sort on the runs created
-    for i in range(0, n, min_run):
-        insertion_sort(arr, i, min((i+min_run-1), n-1))
-        
-    # merge the sorted runs
-    size = min_run
-    while(size < n):
-        for start in range(0, n, size*2):
-            midpoint = start + size - 1
-            end = min((start + size*2 - 1), (n-1))
-            merged_arr = merge(arr, start, midpoint, end)
-            arr[start:start+len(merged_arr)] = merged_arr
+
+def tim_sort(array, unit=32):
+    length = len(array)
+    for i in range(0, length, unit):
+        array = insert_sort(array, i, min(i + unit, length))
+
+    size = unit
+
+    while size < length:
+        for start in range(0, length, size * 2):
+            mid = start + size - 1
+            end = min(start + size * 2 - 1, length)
+            merged_array = merge(array, start, mid, end)
+            array[start: start + len(merged_array)] = merged_array
+
         size *= 2
-    
-    return arr
 
-# Test the above implemented Tim sort algorithm
-arr = [5, 21, 7, 23, 19]
-print(tim_sort(arr))
+    return array
+
+
+# Test Case 1
+arr1 = [3, 2, 1]
+expected_output1 = [1, 2, 3]
+print(tim_sort(arr1) == expected_output1)
+
+# Test Case 2
+arr2 = []
+expected_output2 = []
+print(tim_sort(arr2) == expected_output2)
+
+# Test Case 3
+arr3 = [1, 1, 1]
+expected_output3 = [1, 1, 1]
+print(tim_sort(arr3) == expected_output3)
+
+# Test Case 4
+arr4 = [5, 1, 7, 9, 2, 10]
+expected_output4 = [1, 2, 5, 7, 9, 10]
+print(tim_sort(arr4) == expected_output4)
--- 10.test/1.sort/timsort2.py
+++ 10.test/1.sort/timsort2.py
@@ -1,11 +1,67 @@
-def insert_sort(array: list, left, right):
-    for node in range(left, right):
+def insert_sort(array: list, start, end):
+    for node in range(start, end):
         idx = node
 
-        while idx >= left and array[idx] > array[idx + 1]:
+        while idx >= start and array[idx] > array[idx + 1]:
             array[idx + 1], array[idx] = array[idx], array[idx + 1]
             idx -= 1
 
+    return array
+
+
+def merge(array, start, mid, end):
+    left = array[start: mid]
+    right = array[mid: end]
+
+    sorted_array = []
+
+    while left and right:
+        if left[0] < right[0]:
+            sorted_array.append(left.pop(0))
+        else:
+            sorted_array.append(right.pop(0))
+
+    sorted_array += left
+    sorted_array += right
+
+    return sorted_array
+
+
+def tim_sort(array, unit=32):
+    length = len(array)
+    for i in range(0, length, unit):
+        array = insert_sort(array, i, min(i + unit, length))
+
+    size = unit
+
+    while size < length:
+        for start in range(0, length, size * 2):
+            mid = start + size - 1
+            end = min(start + size * 2 - 1, length)
+            merged_array = merge(array, start, mid, end)
+            array[start: start + len(merged_array)] = merged_array
+
+        size *= 2
+
+    return array
+
+
+# Test Case 1
 arr1 = [3, 2, 1]
-insert_sort(arr1, 0, 2)
-print(arr1)
+expected_output1 = [1, 2, 3]
+print(tim_sort(arr1) == expected_output1)
+
+# Test Case 2
+arr2 = []
+expected_output2 = []
+print(tim_sort(arr2) == expected_output2)
+
+# Test Case 3
+arr3 = [1, 1, 1]
+expected_output3 = [1, 1, 1]
+print(tim_sort(arr3) == expected_output3)
+
+# Test Case 4
+arr4 = [5, 1, 7, 9, 2, 10]
+expected_output4 = [1, 2, 5, 7, 9, 10]
+print(tim_sort(arr4) == expected_output4)
