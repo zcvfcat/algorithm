93cc58eaf8523ba5f8e64009d9e76eea8f80ce01
김대현 <zcvfcat@gmail.com>
2023-04-27 09:14:21 +0900

    feat (bfs)

---
+++ 10.test/4.search/bfs3.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 6, 8],
+    [1, 4, 5],
+    [3],
+    [3],
+    [2, 7],
+    [6],
+    [1, 2],
+]
+
+def bfs(graph, start, end):
+    visited = [0 for _ in range(len(graph))]
+
+    q = deque([start])
+    visited[start] = True
+
+    while q:
+        node = q.popleft()
+
+        if node == end:
+            return visited[end]
+
+        for edge in graph[node]:
+            if visited[edge] == 0:
+                visited[edge] = visited[node] + 1
+                q.append(edge)
+
+    return None
+
+print(bfs(graph, 1, 7))
