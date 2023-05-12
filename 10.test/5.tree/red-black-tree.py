0e8187744586cf10443b4731477dc10797914289
김대현 <zcvfcat@gmail.com>
2023-05-12 09:05:38 +0900

    feat (warm-up)

--- 10.test/5.tree/red-black-tree.py
+++ 10.test/5.tree/red-black-tree.py
@@ -39,4 +39,57 @@ class RedBlackTree:
         self.insert_fix(node)
     
     def insert_fix(self, node: Node):
+        while node.parent != None and node.parent.color == 0:
+            if node.parent == node.parent.parent.left:
+                y = node.parent.parent.right
+
+                if y != None and y.color == 0:
+                    node.parent.color = 1
+                    y.color = 1
+                    node.parent.parent.color = 0
+                    node = node.parent.parent
+                else:
+
+                    if node == node.parent.right:
+                        node = node.parent
+                        self.left_rotate(node)
+                    node.parent.color = 1
+                    node.parent.parent.color = 0
+                    self.right_rotate(node.parent.parent)
+            else:
+                y = node.parent.parent.left
+                if y != None and y.color == 0:
+                    node.parent.color = 1
+                    y.color = 1
+                    node.parent.parent.color = 0
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.right_rotate(node)
+                    node.parent.color = 1
+                    node.parent.parent.color = 0
+                    self.left_rotate(node.parent.parent)
+        self.root.color = 1
+    
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+
+        if y.left != None:
+            y.left.parent = x
+        y.parent = x.parent
+
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, x):
         pass
+    
