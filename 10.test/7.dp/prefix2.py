039e91163d4d9a3e1fa9075dd683890c12b53863
김대현 <zcvfcat@gmail.com>
2023-05-15 09:02:45 +0900

    feat (warm-up)

---
+++ 10.test/7.dp/prefix2.py
@@ -0,0 +1,17 @@
+def prefix2(array):
+    prefix = [[0 for _ in range(len(array[0]) + 1)] for _ in range(len(array) + 1)]
+
+    for h, r in enumerate(array):
+        for w, value in enumerate(r):
+            prefix[h + 1][w + 1] = value + prefix[h + 1][w] \
+                                    + prefix[h][w + 1] - prefix[h][w]
+    return prefix
+
+
+array = [
+    [1, 2, 3, 4, 5],
+    [11, 12, 13, 14, 15],
+    [21, 22, 23, 24, 25],
+    [31, 32, 33, 34, 35],
+]
+print(prefix2(array))
