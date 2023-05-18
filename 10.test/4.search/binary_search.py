00771b51f0ad1ebf898363a0617d70ac05cb2de9
김대현 <zcvfcat@gmail.com>
2023-05-18 09:06:54 +0900

    feat (warm-up)

--- 10.test/4.search/binary_search.py
+++ 10.test/4.search/binary_search.py
@@ -11,5 +11,28 @@ def recur_search(array, target, start, end):
     else:
         return recur_search(array, target, mid + 1, end)
 
-array = [0, 1, 3, 6, 7, 8, 9]
+
+array = [0, 1, 3, 6, 7, 8, 8, 9]
 print(recur_search(sorted(array), 8, 0, len(array)))
+
+
+def search(array, target):
+    array = sorted(array)
+
+    start = 0
+    end = len(array)
+
+    while start < end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return None
+
+
+print(search(array, 8))
