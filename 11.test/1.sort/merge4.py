65191479ec0e7d76e24fcd21157de0fd5dbc5e2d
김대현 <zcvfcat@gmail.com>
2023-05-31 09:03:13 +0900

    feat (warm-up)

---
+++ 11.test/1.sort/merge4.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def merge(array):
+    if len(array) < 2:
+        return array
+    
+    mid = len(array) // 2
+    
+    left, right = deque(merge(array[:mid])), deque(merge(array[mid:]))
+
+    merged = []
+    while left and right:
+        if left[0] < right[0]:
+            merge += [left.popleft()]
+        elif right[0] < left[0]:
+            merge += [right.popleft()]
+    
+    merged += left
+    merged += right
+
+    return array
+
+
+# Test Case 1
+arr1 = [3, 2, 1]
+expected_output1 = [1, 2, 3]
+print(merge(arr1) == expected_output1)
+
+# Test Case 2
+arr2 = []
+expected_output2 = []
+print(merge(arr2) == expected_output2)
+
+# Test Case 3
+arr3 = [1, 1, 1]
+expected_output3 = [1, 1, 1]
+print(merge(arr3) == expected_output3)
+
+# Test Case 4
+arr4 = [5, 1, 7, 9, 2, 10]
+expected_output4 = [1, 2, 5, 7, 9, 10]
+print(merge(arr4) == expected_output4)
+
