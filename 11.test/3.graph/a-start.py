c506b4202ffed25fff9228bcce6089ee0dd7c6f3
김대현 <zcvfcat@gmail.com>
2023-06-26 09:04:55 +0900

    feat (warm-up)

--- 11.test/3.graph/a-start.py
+++ 11.test/3.graph/a-start.py
@@ -5,8 +5,16 @@ from math import inf, sqrt
 def astar(minimap: list, start_y, start_x, end_y, end_x):
     distances = [[inf for _ in range(len(minimap))] for _ in range(len(minimap[0]))]
     distances[start_y][start_x] = 0
-    
+
     h = []
+    
+    queue = [(start_y, start_x, 0)]
+
+    while queue:
+        y, x, cost = heapq.heappop(queue)
+        
+        if y == end_y and x == end_x:
+            break
 
 
 
@@ -25,3 +33,5 @@ minimap = [
 ]
 start = (0, 0)
 end = (4, 4)
+
+astar(minimap, start[0], start[1], end[0], end[1])
