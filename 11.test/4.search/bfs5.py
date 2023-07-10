3ba82b1d08db15775565df2f279c022517c11551
김대현 <zcvfcat@gmail.com>
2023-07-10 09:02:14 +0900

    feat (warm-up)

---
+++ 11.test/4.search/bfs5.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def bfs(graph, start_node):
+    visited = [0 for _ in range(len(graph))]
+    q = deque([start_node])
+
+    while q:
+        node = q.popleft()
+
+        for edge in graph[node]:
+            if visited[edge] == 0:
+                q += [edge]
+                visited[edge] = visited[node] + 1
+        
+    return visited
