3cca88f4180f0590d51fd21e2c26adc4bbaa8e9d
김대현 <zcvfcat@gmail.com>
2023-08-18 09:00:21 +0900

    feat (warm-up)

---
+++ 3.graph/prim.py
@@ -0,0 +1,41 @@
+from heapq import heappush, heappop
+
+
+def prim(graph, start_node):
+    visited = [False for _ in range(len(graph))]
+    visited[start_node] = 0
+
+    mst = []
+
+    q = []
+    for edge, cost in graph[start_node]:
+        heappush(q, (cost, start_node, edge))
+
+    while q:
+        cost, node, edge = heappop(q)
+
+        if visited[edge] is True:
+            continue
+
+        visited[edge] = True
+        mst.append((node, edge, cost))
+
+        for next_edge, weight in graph[edge]:
+            if not visited[edge]:
+                heappush(q, (weight, edge, next_edge))
+
+    return mst
+
+
+# 그래프: (정점, 정점, 가중치)
+graph = {
+    0: [(1, 4), (2, 3)],
+    1: [(0, 4), (2, 2), (3, 1)],
+    2: [(0, 3), (1, 2), (3, 5)],
+    3: [(1, 1), (2, 5)]
+}
+
+mst = prim(graph, 0)
+
+for edge in mst:
+    print(edge)
