e37cbdb675e9725f6d946d4153071427d8252ecc
김대현 <zcvfcat@gmail.com>
2023-08-29 09:03:11 +0900

    feat (warm-up)

---
+++ 3.graph/mst(kruskal).py
@@ -0,0 +1,27 @@
+def mst(graph: list[tuple[int, int]]):
+    parents = [i for i in range(len(graph))]
+
+    def find(node):
+        if parents[node] != node:
+            parents[node] = find(parents[node])
+        return parents[node]
+
+    def union(a, b):
+        a = find(a)
+        b = find(b)
+
+        if a != b:
+            parents[a] = b
+
+    edges = []
+    total = 0
+    for node, node_edges in enumerate(graph):
+        for edge, cost in node_edges:
+            edges.append((cost, node, edge))
+
+    for cost, node, edge in edges:
+        if find(node) != find(edge):
+            union(node, edge)
+            total += cost
+
+    return total
