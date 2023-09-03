057f0c2bc89b8f049c603947a8c420c65808c4db
zcvfcat <zcvfcat@gmail.com>
2023-09-03 13:25:28 +0900

    feat (graph)

--- 0.data-structure/3.graph/3.weighted-graph.py
+++ 0.data-structure/3.graph/3.weighted-graph.py
@@ -1,3 +1,23 @@
 """
     가중치 그래프
 """
+
+class WeightedGraph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.graph:
+            self.graph[vertex] = {}
+
+    def add_edge(self, vertex1, vertex2, weight):
+        self.add_vertex(vertex1)
+        self.add_vertex(vertex2)
+        self.graph[vertex1][vertex2] = weight
+        self.graph[vertex2][vertex1] = weight  # 무방향 그래프인 경우
+
+    def get_neighbors(self, vertex):
+        if vertex in self.graph:
+            return self.graph[vertex]
+        else:
+            return {}
--- 0.data-structure/3.graph/4.cycle-graph.py
+++ 0.data-structure/3.graph/4.cycle-graph.py
@@ -1,3 +1,14 @@
 """
     사이클 그래프
 """
+
+
+class CycleGraph:
+    def __init__(self, size):
+        self.size = size
+        self.graph = [[] for _ in range(size)]
+
+    def add_edge(self, node, edge):
+        if 0 <= node < len(self.graph) and 0 <= edge < len(self.edge):
+            self.graph[node].append(edge)
+            self.graph[edge].append(node)
