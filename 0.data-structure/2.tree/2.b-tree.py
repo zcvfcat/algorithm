c808d26e934ed18e51c3f1cefc45d7cfa91359cb
김대현 <zcvfcat@gmail.com>
2023-09-12 09:02:13 +0900

    feat (warm-up)

--- 0.data-structure/2.tree/2.b-tree.py
+++ 0.data-structure/2.tree/2.b-tree.py
@@ -23,13 +23,97 @@
 
 
 class BTreeNode:
-    def __init__(self, keys, children, is_leaf) -> None:
-        self.keys = keys
-        self.children = children
-        self.is_leaf = is_leaf
+    def __init__(self, leaf=False):
+        self.keys = []
+        self.child = []
+        self.leaf = leaf
 
 
 class BTree:
-    def __init__(self, t) -> None:
-        self.root = BTreeNode([], [], True)
+    def __init__(self, t):
+        self.root = BTreeNode(True)
         self.t = t
+
+    def insert(self, k):
+        root = self.root
+
+        if len(root.keys) == (2 * self.t) - 1:
+            temp = BTreeNode()
+            self.root = temp
+            temp.child.insert(0, root)
+            self.split_child(temp, 0)
+            self.insert_non_full(temp, k)
+        else:
+            self.insert_non_full(root, k)
+
+    def insert_non_full(self, x, k):
+        i = len(x.keys) - 1
+        print(x.leaf)
+        if x.leaf:
+            x.keys.append((None, None))
+            while i >= 0 and k < x.keys[i]:
+                x.keys[i + 1] = x.keys[i]
+                i -= 1
+            x.keys[i + 1] = k
+        else:
+            while i >= 0 and k < x.keys[i]:
+                i -= 1
+
+            i += 1
+            if len(x.child[i].keys) == (2 * self.t) - 1:
+                self.split_child(x, i)
+                if k > x.keys[i]:
+                    i += 1
+            self.insert_non_full(x.child[i], k)
+
+    def split_child(self, x, i):
+        t = self.t
+        y = x.child[i]
+        z = BTreeNode(y.leaf)
+
+        x.child.insert(i + 1, z)
+        x.keys.insert[t: (2 * t) - 1]
+        y.keys = y.keys[0: t - 1]
+
+        if not y.leaf:
+            z.child = y.child[t: 2 * t]
+            y.child = y.child[0: t - 1]
+
+    def search(self, k, x=None):
+        if isinstance(x, BTreeNode):
+            i = 0
+            while i < len(x.keys) and k > x.keys[i]:
+                i += 1
+
+            if i < len(x.keys) and k == x.keys[i]:
+                return x
+            elif x.leaf:
+                return None
+            else:
+                return self.search(k, x.child[i])
+        else:
+            return self.search(k, self.root)
+
+
+btree = BTree(3)  # t = 3으로 설정된 B-트리 생성
+
+# 키 삽입
+btree.insert(10)
+btree.insert(20)
+btree.insert(5)
+btree.insert(15)
+btree.insert(25)
+
+# 특정 키 검색
+result = btree.search(15)
+if result:
+    print("15를 찾았습니다")
+else:
+    print("15를 찾을 수 없습니다")
+
+# 존재하지 않는 키 검색
+result = btree.search(30)
+if result:
+    print("30을 찾았습니다")
+else:
+    print("30을 찾을 수 없습니다")
