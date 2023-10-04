a3f40a29936118bd6dc88eb7f206ffa1f1a659d8
김대현 <zcvfcat@gmail.com>
2023-10-05 08:58:23 +0900

    feat (warm-up)

--- _.test/floyd.py
+++ _.test/floyd.py
@@ -1,19 +1,24 @@
 from math import inf
 
-
+# O(v^3)
 def floyd(graph):
+    # 2차 인접 행렬
     distances = [[inf for _ in range(len(graph))] for _ in range(len(graph))]
 
     for node in range(len(graph)):
         distances[node][node] = 0
-
+    
+    # graph 2차 배열
+    # (edge, cost)
     for node, edges in enumerate(graph):
-        for cost, edge in edges:
+        for edge, cost in edges:
             distances[node][edge] = cost
-
+    
     for route in range(len(graph)):
         for node in range(len(graph)):
             for edge in range(len(graph)):
                 distances[node][edge] = min(distances[node][edge], distances[node][route] + distances[route][edge])
     
+    # 2차 인접 행렬 (node -> edge 최소비용 리턴)
     return distances
+
