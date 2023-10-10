13c77f1bdc2b4b90acfceeb54528a69e50c0fad8
김대현 <zcvfcat@gmail.com>
2023-10-10 15:55:06 +0900

    feat (warm-up)

--- _.test/bfs.left.py
+++ _.test/bfs.left.py
@@ -4,20 +4,19 @@ d = [(0, -1), (-1, 0), (0, 1), (1, 0)]
 
 
 def solution(arr: list[list[int]]):
-    distances = [[0 for _ in range(len(arr[0]))] for _ in range(len(arr))]
     h, w = len(arr), len(arr[0])
     end = (h - 1, w - 1)
 
     def in_boundary(y, x):
         return 0 <= y < h and 0 <= x < w
 
-    q = deque([(0, 0, 0)])
+    q = deque([(0, 0, 0, 1)])
 
     while q:
-        y, x, d_i = q.popleft()
+        y, x, d_i, cost = q.popleft()
 
         if (y, x) == end:
-            return distances[y][x]
+            return cost
 
         for i in range(4):
             curr_i, prev_i = (d_i + i) % 4, (d_i + i - 1) % 4
@@ -33,14 +32,12 @@ def solution(arr: list[list[int]]):
 
             # case 1, 벽이 존재할 경우
             if in_boundary(ny, nx) and (not in_boundary(kky, kkx) or arr[kky][kkx] == 1):
-                distances[ny][nx] = distances[y][x] + 1
-                q.append((ny, nx, curr_i))
+                q.append((ny, nx, curr_i, cost + 1))
                 break
 
             # case 2, 벽이 없을 경우
             if in_boundary(kky, kkx) and arr[kky][kkx] == 0:
-                distances[kky][kkx] = distances[y][x] + 1
-                q.append((kky, kkx, prev_i))
+                q.append((kky, kkx, prev_i, cost + 1))
                 break
 
     return None
