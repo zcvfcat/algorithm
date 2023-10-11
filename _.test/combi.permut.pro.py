16281aa4319da4f3ee2dd496edebb6287853893c
김대현 <zcvfcat@gmail.com>
2023-10-11 09:00:30 +0900

    feat (warm-up)

---
+++ _.test/combi.permut.pro.py
@@ -0,0 +1,14 @@
+from itertools import permutations, combinations, product
+
+k = ['2', '0', '0', "0", '1']
+l = len(k)
+
+print('nPr 순서가 필요한 경우')
+print([*permutations(k, l)])
+
+
+print('nCr 순서가 필요없는 경우')
+print([*combinations(k, l)])
+
+print('product')
+print(product(k, l))
