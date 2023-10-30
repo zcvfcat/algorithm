b265fdf850b89ca39cf654c6c30d96df8a2a8279
김대현 <zcvfcat@gmail.com>
2023-10-30 09:01:06 +0900

    feat (warm-up)

---
+++ _.test/floyd-warshall.py
@@ -0,0 +1,18 @@
+from math import inf
+
+def floyd(graph):
+    distances = [[inf for _ in range(len(graph))] for _ in range(len(graph))]
+
+    for node in range(len(graph)):
+        distances[node][node] = 0
+    
+    for node, edges in enumerate(graph):
+        for edge, cost in edges:
+            distances[node][edge] = cost
+        
+    for route in range(len(graph)):
+        for node in range(len(graph)):
+            for edge in range(len(graph)):
+                distances[node][edge] = min(distances[node][edge], distances[node][route] + distances[route][edge])
+
+    return distances
