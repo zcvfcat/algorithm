a1d6d3488c3663c1b70791f966898ad03cb7ef26
김대현 <zcvfcat@gmail.com>
2023-11-14 08:51:32 +0900

    feat (quick, merge)

---
+++ _.test/quick.merge.2.py
@@ -0,0 +1,33 @@
+def quick(arr):
+    if len(arr) < 2:
+        return arr
+    
+    pivot = arr[len(arr)//2]
+
+    left, equal, right = [],[],[]
+
+    for v in arr:
+        if v < pivot:
+            left.append(v)
+        elif v > pivot:
+            right.append(v)
+        else:
+            equal.append(v)
+
+    return quick(left) + equal + quick(right)
+
+def merge(arr):
+    if len(arr)< 2:
+        return arr
+
+    mid = len(arr) // 2
+    left, right, merged = merge(arr[:mid]), merge(arr[mid:]), []
+
+    while left and right:
+        if left[0] > right[0]:
+            merged.append(right.pop(0))
+        else:
+            merged.append(left.pop(0))
+    
+    return merged + left + right
+
