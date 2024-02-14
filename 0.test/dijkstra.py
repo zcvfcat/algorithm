3bd9f7080a6b58364e18e172b9e15393c85aa2be
김대현 <zcvfcat@gmail.com>
2024-02-15 08:58:55 +0900

    feat (warm-up)

---
+++ 0.test/dijkstra.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+from math import inf
+
+def dijkstra(graph,start,end):
+    distance = [inf for _ in range(len(graph))]
+    distance[start] = 0
+    q = []
+
+    heappush(q,(0, start))
+
+    while q:
+        cost, node = heappop(q)
+
+        if distance[node] < cost:
+            continue
+
+        for edge, weight in graph[node]:
+            next_cost = cost + weight
+
+            if distance[edge] > next_cost:
+                distance[edge] = next_cost
+                heappush(q, (next_cost, edge))
+    
+    return distance
