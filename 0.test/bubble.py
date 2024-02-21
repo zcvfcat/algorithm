3413dabc6f982f44b0f802750ecc4ed1a59166b5
김대현 <zcvfcat>
2024-02-22 08:45:00 +0900

    feat (warm-up)

---
+++ 0.test/bubble.py
@@ -0,0 +1,7 @@
+def bubble(arr):
+    for l in range(len(arr) - 1, -1, -1):
+        for i in range(l):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    
+    return arr
