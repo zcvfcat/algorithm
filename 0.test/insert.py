9d21b661c0f7fa39e5ed69e52c599bcd47f9b18c
김대현 <zcvfcat>
2024-02-26 08:43:14 +0900

    feat (warm-up)

---
+++ 0.test/insert.py
@@ -0,0 +1,8 @@
+def insert(arr):
+    for i in range(len(arr)):
+        while i > 0 and arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            i -= 1
+    return arr
+
+print(insert([5,4,3,2,1]))
