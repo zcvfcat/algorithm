1fe39dfc299ebc9f829df9775f00818950a8cf3a
김대현 <zcvfcat@gmail.com>
2024-04-16 08:50:42 +0900

    feat (warm-up)

--- 0.test/dfs.py
+++ 0.test/dfs.py
@@ -1,18 +1,12 @@
-from collections import deque
+def dfs(graph: list[int], visited: list[int], node: int, end: int):
+    if node == end:
+        return visited[end]
 
+    for edge in graph[node]:
+        if visited[edge] == -1:
+            visited[edge] = visited[node] + 1
+            result = dfs(graph, visited, edge, end)
+            if result != None:
+                return result
 
-def dfs(graph, start, end):
-    visited = [-1 for _ in range(len(graph))]
-    visited[start] = 0
-
-    q = deque([start])
-
-    while q:
-        node = q.popleft()
-
-        for edge in graph[node]:
-            if visited[edge] == -1:
-                visited[edge] = visited[node] + 1
-                q.append(edge)
-
-    return visited[end]
+    return None
