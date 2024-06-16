4e065b861d2038a5f77bfa6b1d7df57fc94073c3
김대현 <zcvfcat@gmail.com>
2024-06-17 08:53:03 +0900

    feat (warm-up)

---
+++ 0.test/quick.7.py
@@ -0,0 +1,16 @@
+def quick(array):
+    if len(array) < 2:
+        return array
+    
+    pivot = array[len(array) // 2]
+    left, equal, right = [], [], []
+
+    for v in array:
+        if v < pivot:
+            left.append(v)
+        elif v > pivot:
+            right.append(v)
+        else:
+            equal.append(v)
+    
+    return quick(left) + equal + quick(right)
