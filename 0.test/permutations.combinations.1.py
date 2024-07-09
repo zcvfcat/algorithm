71c5b99623dfd6a308ba33f89d76f12a5e673708
김대현 <zcvfcat@gmail.com>
2024-07-09 09:00:41 +0900

    feat (warm-up)

---
+++ 0.test/permutations.combinations.1.py
@@ -0,0 +1,27 @@
+def permutations(array, ties):
+    if ties < 1:
+        return [[]]
+
+    merged = []
+
+    for node, value in enumerate(array):
+        for edges in permutations(array[node + 1:] + array[:node], ties - 1):
+            merged.append((value, *edges))
+    return merged
+
+
+def combinations(array, ties):
+    if ties < 1:
+        return [[]]
+
+    merged = []
+
+    for node, value in enumerate(array):
+        for edges in combinations(array[node + 1:], ties - 1):
+            merged.append((value, *edges))
+
+    return merged
+
+
+# print(permutations([10, 3, 2],2))
+print(combinations([10, 3, 2],2))
