de17027f08cef7d4dea0ae2bc5611e3d178e6641
김대현 <zcvfcat@gmail.com>
2024-07-11 09:21:31 +0900

    feat (warm-up)

---
+++ 0.test/mst.4.py
@@ -0,0 +1,28 @@
+def mst(graph: list[int, int]):
+    parents = [i for i in graph(graph)]
+
+    def find(a, b):
+        if parents[a] != a:
+            parents[a] = find(parents[a])
+        return parents[a]
+
+    def union(a, b):
+        a, b = find(a), find(b)
+        if a != b:
+            parents[b] = a
+
+    edges = []
+    for node, connections in enumerate(graph):
+        for neighbor, weight in connections.items():
+            edges.append((weight, node, neighbor))
+
+    edges.sort()
+    total = 0
+
+    for edge in edges:
+        weight, node, neighbor = edge
+        if find(node) != find(neighbor):
+            total += weight
+            union(node, neighbor)
+
+    return total
