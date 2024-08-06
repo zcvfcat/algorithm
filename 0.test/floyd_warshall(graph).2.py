b456026d3d40a41deecf3d80a7254188377d8460
김대현 <zcvfcat@gmail.com>
2024-08-07 08:54:01 +0900

    feat (warm-up)

---
+++ 0.test/floyd_warshall(graph).2.py
@@ -0,0 +1,14 @@
+def floyd_warshall(graph:list[int]):
+    distances = [[float('inf') for i in range(len(graph))] for i in range(len(graph))]
+
+    for node in range(len(graph)):
+        distances[node][node] = 0
+        for edge, cost in graph[node]:
+            distances[node][edge] = cost
+
+    for route in range(len(graph)):
+        for node in range(len(graph)):
+            for dest in range(len(graph)):
+                distances[node][dest] = min(distances[node][dest], distances[node][route] + distances[route][dest])
+
+    return distances
