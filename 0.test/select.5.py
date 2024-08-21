749f75f1060997f0152b54a4f1da9158a0bc51fc
김대현 <zcvfcat@gmail.com>
2024-08-22 08:58:12 +0900

    feat (warm-up)

---
+++ 0.test/select.5.py
@@ -0,0 +1,9 @@
+def select(array):
+    for i in range(len(array)):
+        cur = i
+        for k in range(i + 1, len(array)):
+            if array[cur] > array[k]:
+                cur = k
+        array[cur], array[i] = array[i], array[cur]
+
+    return array
