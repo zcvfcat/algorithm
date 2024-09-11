3702267e995be1acf0f58197aae06c8984a8656e
김대현 <zcvfcat@gmail.com>
2024-09-11 09:00:22 +0900

    feat (warm-up)

---
+++ 0.test/merge.13.py
@@ -0,0 +1,15 @@
+def merge(array):
+    if len(array) < 2:
+        return array
+    
+    mid = len(array) // 2
+    left, right = merge(array[:mid]), merge(array[mid:])
+    merged = []
+
+    while left and right:
+        if left[0] < right[0]:
+            merged.append(left.pop(0))
+        else:
+            merged.append(right.pop(0))
+    
+    return merged + left + right
