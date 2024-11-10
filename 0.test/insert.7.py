56e5e854f535d8e9e62e46638b4f7ec4444c7d94
김대현 <zcvfcat@gmail.com>
2024-11-11 08:52:12 +0900

    feat (warm-up)

---
+++ 0.test/insert.7.py
@@ -0,0 +1,6 @@
+def insert(array):
+    for i in range(len(array)):
+        while i > 0 and array[i] != array[i-1]:
+            array[i], array[i-1] = array[i-1], array[i]
+            i -= 1
+    return array
